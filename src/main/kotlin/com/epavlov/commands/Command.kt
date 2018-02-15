2ef825313a8a704c3a5473ae29ee4da39c4c41ea
epavlov <epavlov@reksoft.ru>
2018-02-15 14:29:24 +0300

    add error saver

--- src/main/kotlin/com/epavlov/commands/Command.kt
+++ src/main/kotlin/com/epavlov/commands/Command.kt
@@ -1,5 +1,5 @@
 package com.epavlov.commands
 
 enum class Command{
-    GET_TRACK, GET_TRACK_ID, POST, DELETE
+    GET_TRACK, GET_TRACK_ID, POST, DELETE, ADD_DESC
 }
---
+++ src/main/kotlin/com/epavlov/commands/desc/DescriptionHelper.kt
@@ -0,0 +1,42 @@
+package com.epavlov.commands.desc
+
+import com.epavlov.PropReader
+import com.epavlov.bot.BotImpl
+import com.epavlov.dao.UserDAO
+import kotlinx.coroutines.experimental.launch
+import org.apache.log4j.LogManager
+import org.telegram.telegrambots.api.methods.send.SendMessage
+
+object DescriptionHelper {
+    private val log = LogManager.getLogger(DescriptionHelper::class.java)
+
+    private val userIdTrackIdmap = HashMap<Long, String>()
+
+
+    fun isDesc(userId: Long): Boolean = userIdTrackIdmap.containsKey(userId)
+
+    fun addToMap(userId: Long, trackId: String) {
+        log.debug("[addToMap] <$userId,$trackId>")
+        userIdTrackIdmap[userId] = trackId
+        BotImpl.sendMessageToUser(SendMessage(userId,PropReader.getProperty("INPUT_DESC")))
+    }
+
+    fun saveDesc(userId: Long, text: String) {
+        log.debug("[saveDesc] $userId: $text")
+        if (isDesc(userId)) {
+            val trackId = userIdTrackIdmap[userId]
+            launch {
+                val user = UserDAO.get(userId)
+                user?.trackList!![trackId]?.let {
+                    it.name = text
+                    userIdTrackIdmap.remove(userId)
+                    UserDAO.save(user)
+                    BotImpl.sendMessageToUser(SendMessage(userId,PropReader.getProperty("DESC_CHANGED")))
+                }
+            }
+        } else {
+            log.error("user don't have this description")
+        }
+    }
+
+}
